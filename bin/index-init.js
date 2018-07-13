0d24de9060a56ce76d478059c9eb354a732f078a
LengYingxin <lengyingxin8966@gmail.com>
2018-07-13 14:20:28 +0800

    1

--- bin/index-init.js
+++ bin/index-init.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 const program = require('commander')
+const download = require('../lib/download');
+download();
+// program.usage('<project-name>').parse(process.argv)
 
-program.usage('<project-name>').parse(process.argv)
+// // 根据输入，获取项目名称
+// let projectName = program.args[0]
 
-// 根据输入，获取项目名称
-let projectName = program.args[0]
+// if (!projectName) {  // project-name 必填
+//   // 相当于执行命令的--help选项，显示help信息，这是commander内置的一个命令选项
+//   program.help() 
+//   return
+// }
 
-if (!projectName) {  // project-name 必填
-  // 相当于执行命令的--help选项，显示help信息，这是commander内置的一个命令选项
-  program.help() 
-  return
-}
+// go()
 
-go()
-
-function go () {
-	// 预留，处理子命令  
-}
+// function go () {
+// 	// 预留，处理子命令  
+// }
