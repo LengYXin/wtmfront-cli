02e5a9c618fe8798f3bbf0f99cec84abfbebb5a6
LengYingxin <lengyingxin8966@gmail.com>
2018-07-13 14:01:42 +0800

    模板

--- bin/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const program = require('commander')  // npm i commander -D
-
-program.version('1.0.0')
-	.usage('<command> [项目名称]')
-    .command('hello', 'hello')
-	.command('init', 'init')
-	.parse(process.argv)
