ba79b563fb7c696a531af5cbc87de63a613d1fdf
LengYingxin <lengyingxin8966@gmail.com>
2018-07-17 16:15:06 +0800

    1

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander')  // npm i commander -D
 
-program.version('0.0.1')
+program.version('0.0.1','-v, --version')
 	.usage('<command> [项目名称]')
     .command('hello', 'hello')
 	.command('init', 'init')
