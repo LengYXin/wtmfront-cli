e75e876b0a3a76ecf9eac7b344b3410393bf104d
LengYingxin <lengyingxin8966@gmail.com>
2018-07-26 16:37:02 +0800

    version

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const program = require('commander')  // npm i commander -D
-
-program.version('0.0.1','-v, --version')
+const package = require('../package.json');
+program.version(package.version, '-v, --version')
 	.usage('<command> [项目名称]')
-    .command('hello', 'hello')
+	.command('hello', 'hello')
 	.command('init', 'init')
 	.command('server', 'server')
 	.parse(process.argv)
